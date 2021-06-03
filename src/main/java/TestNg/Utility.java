acd84d8d0c26a4ad958c3879b4b18e4861b1d11c
nimishgarg5 <nimishgarg5@gmail.com>
2021-06-03 12:23:33 +0530

    Added wait

--- src/main/java/TestNg/Utility.java
+++ src/main/java/TestNg/Utility.java
@@ -6,14 +6,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Utility {
-    public String utilityM()
-    {
+    public String utilityM() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\HP\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.in/");
         driver.findElement(By.xpath("//*[@id=\"nav-link-accountList-nav-line-1\"]")).click();
         driver.findElement(By.xpath("//*[@id=\"ap_email\"]")).sendKeys("9660553898");
         driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+        Thread.sleep(4000);
         driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys("Nimish123");
         driver.findElement(By.xpath("//*[@id=\"signInSubmit\"]")).click();
         String tittle = driver.getTitle();
